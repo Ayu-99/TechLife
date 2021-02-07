2b1927ac56442671f25619ee4cd008f1807d934d
Your Name <ayushisharma5141@gmail.com>
2021-02-07 19:42:19 +0530

    first commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import '../src/index.css';
 import {NavLink} from "react-router-dom";
-import "./img.jpg";
+import  img from "./img.jpg";
 
 const Card=()=>{
     return (
         <>
             <div className="col-md-4 col-10 mx-auto">
                 <div className="card">
-                <img className="card-img-top" src="img.jpg" alt="image"/>
+                <img className="card-img-top" src={img} alt="image"/>
                 <div className="card-body">
                     <h5 className="card-title">Android</h5>
                     <p className="card-text">Android software development is the process by which applications are created for devices running the Android operating system.</p>
